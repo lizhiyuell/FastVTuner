46bd3dba89d5efbebfad424e47b419754fadfb88
lizhiyuell <sldjlzy@163.com>
2026-04-30 07:59:50 +0000

    remove vector-db-benchmark from project

--- vector-db-benchmark/engine/clients/qdrant_native/__init__.py
+++
@@ -1,9 +0,0 @@
-from .configure import QdrantNativeConfigurator
-from .search import QdrantNativeSearcher
-from .upload import QdrantNativeUploader
-
-__all__ = [
-    "QdrantNativeConfigurator",
-    "QdrantNativeUploader",
-    "QdrantNativeSearcher",
-]
